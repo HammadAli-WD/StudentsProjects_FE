89b94bb0a426d660a7d8e8f0ee42a80409ea54fa
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-12-19 12:58:52 +0100

    redux store

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import reducer from "./reducers";
+import thunk from "redux-thunk";
+const initialState = {
+    loading: true,
+    data: [],
+    student: {},
+    error: null,
+};
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () =>
+    composeEnhancers(applyMiddleware(thunk))(createStore)(reducer, initialState);
